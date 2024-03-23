dd3661a831009e1f62890dd2704f41ffb1b5cb90
Diego França <diego.franca92@gmail.com>
2024-03-23 19:09:46 -0300

    inicial structure with first layout

---
+++ app.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <primary-layout></primary-layout>
+  </div>
+</template>
+<script>
+import PrimaryLayout from './src/layouts/PrimaryLayout'
+import { onMounted } from 'vue'
+import {
+  initAccordions,
+  initCarousels,
+  initCollapses,
+  initDials,
+  initDismisses,
+  initDrawers,
+  initDropdowns,
+  initModals,
+  initPopovers,
+  initTabs,
+  initTooltips
+} from 'flowbite'
+
+onMounted(() => {
+  initAccordions()
+  initCarousels()
+  initCollapses()
+  initDials()
+  initDismisses()
+  initDrawers()
+  initDropdowns()
+  initModals()
+  initPopovers()
+  initTabs()
+  initTooltips()
+})
+
+export default {
+  components: {
+    PrimaryLayout
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/**/*.{js,vue,ts}",
+    "./src/layouts/**/*.vue",
+    "./src/pages/**/*.vue",
+    "./src/plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+    "./node_modules/flowbite/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
